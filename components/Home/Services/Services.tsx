3acaf19f411f66e686573f6e95b53f8a0898bee8
Ali Irfan <maliirfan134@gmail.com>
2025-09-09 11:51:41 +0500

    Update Services.tsx

--- components/Home/Services/Services.tsx
+++ components/Home/Services/Services.tsx
@@ -7,7 +7,7 @@ const Services = () => {
         <h1 className='text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>Colleborate with brand <br /> and agencies to create <br /> impactful results</h1> 
         <div className='w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4gap-10 mt-20 items-center'>
           <div>
-            <ServiceCard icon="/images/s1.png"
+            <ServiceCard icon="/images/s1.PNG"
             name="Web and Mobile App" 
             description="Designing interfaces that are intuitive, efficient, and enjoyable to use." />
           </div>
@@ -17,7 +17,7 @@ const Services = () => {
             description="Designing interfaces that are intuitive, efficient, and enjoyable to use." />
           </div>
           <div>
-            <ServiceCard icon="/images/s3.png"
+            <ServiceCard icon="/images/s3.PNG"
             name="Development" 
             description="Designing interfaces that are intuitive, efficient, and enjoyable to use." />
           </div>
