36910be0c5c62e1f2eb3f4ba3b55b0cd27eabb37
Ali Irfan <maliirfan134@gmail.com>
2025-09-09 12:18:17 +0500

    Update ClientReviewCard.tsx

--- components/Home/ClientReview/ClientReviewCard.tsx
+++ components/Home/ClientReview/ClientReviewCard.tsx
@@ -13,7 +13,7 @@ const ClientReviewCard = ( {image, role, name }: props ) => {
     <div className='m-2'>
         <Image src={image} alt='client' width={60} height={60} className='rounded-full' />
         <p className='mt-6 text-base text-gray-200 font-medium'>
-        &quote;Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, debitis iure tempore culpa exercitationem architecto mollitia at temporibus modi ducimus ipsa ad incidunt consectetur quae! Accusamus veniam aspernatur cum alias.&quote;
+        {"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, debitis iure tempore culpa exercitationem architecto mollitia at temporibus modi ducimus ipsa ad incidunt consectetur quae! Accusamus veniam aspernatur cum alias"}
         </p>
         <h1 className='mt-6 text-xl font-bold text-cyan-200'>{name}</h1>
         <p className='m-1 text-white'>{role}</p>      
